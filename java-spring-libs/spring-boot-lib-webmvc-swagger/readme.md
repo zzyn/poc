de084e564510e835d3864baf29e5762214453bcf
Zack Zou <zack.zou@ef.com>
2021-01-16 15:49:00 +0800

    add webmvc swagger

---
+++ java-spring-libs/spring-boot-lib-webmvc-swagger/readme.md
@@ -0,0 +1,26 @@
+# Spring Boot WebMvc Swagger Library
+
+### Latest Version
+1.0.0
+
+### Usage
+
+Add dependency in pom.xml
+
+```
+<dependency>
+  <groupId>com.spring.libs</groupId>
+  <artifactId>spring-boot-mvc-swagger</artifactId>
+  <version>1.0.0</version>
+</dependency>
+```
+
+Change configuration(application.yml) to enable swagger.
+
+```
+swagger:
+  enabled: true
+  basepackage: xxx.xxx.basepackage
+  title: xxx api
+  version: x.x.x
+```
---
+++ java-spring-libs/spring-boot-lib-webmvc-swagger/src/main/java/com/spring/libs/mvc/swagger/config/SwaggerAutoConfiguration.java
@@ -0,0 +1,48 @@
+package com.spring.libs.mvc.swagger.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@ConditionalOnProperty(
+        prefix = "swagger",
+        value = "enabled"
+)
+@EnableSwagger2
+public class SwaggerAutoConfiguration {
+    @Value("${swagger.base-package}")
+    private String basePackage;
+
+    @Value("${swagger.title}")
+    private String title;
+
+    @Value("${swagger.version}")
+    private String version;
+
+    @Bean
+    public Docket docketInfo() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .useDefaultResponseMessages(false)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage(basePackage))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title(title)
+                .version(version)
+                .build();
+    }
+}
